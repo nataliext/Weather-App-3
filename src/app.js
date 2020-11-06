0d00a979b873af7faf7e647375728b8f0780429f
nataliext <71639472+nataliext@users.noreply.github.com>
2020-11-06 19:06:43 +0000

    Updated icon js

--- src/app.js
+++ src/app.js
@@ -30,7 +30,33 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#windSpeed");
-  let iconElement = document.querySelector("#icon");
+  let icon = document.querySelector("#icon");
+  let code = response.data.weather[0].icon;
+  if (code === "11d") {
+    icon.setAttribute("src", "images/thunderstorm.png");
+  } else if (code === "09d") {
+    icon.setAttribute("src", "images/rain.png");
+  } else if (code === "10d") {
+    icon.setAttribute("src", "images/showerain.png");
+  } else if (code === "09d") {
+    icon.setAttribute("src", "images/rain.png");
+  } else if (code === "13d") {
+    icon.setAttribute("src", "images/snow.png");
+  } else if (code === "50d") {
+    icon.setAttribute("src", "images/mist.png");
+  } else if (code === "01d") {
+    icon.setAttribute("src", "images/sun.png");
+  } else if (code === "01n") {
+    icon.setAttribute("src", "images/moon.png");
+  } else if (code === "02d") {
+    icon.setAttribute("src", "images/fewclouds.png");
+  } else if (code === "02n") {
+    icon.setAttribute("src", "images/moon.png");
+  } else if (code === "03d" || code === "03n") {
+    icon.setAttribute("src", "images/scatterclouds.png");
+  } else if (code === "04d" || code === "04n") {
+    icon.setAttribute("src", "images/brokenclouds.png");
+  }
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -38,11 +64,11 @@ function displayTemperature(response) {
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${code}@2x.png`
   );
 }
 
-let city = "Birmingham";
+let city = "sydney";
 let apiKey = "f0efa6d2628a33743d96ce2e48f71ed5";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
